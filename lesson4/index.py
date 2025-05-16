ce32b7ea2de5006890a00ea0078f047865ec0509
Chiwei-star <panda2008k@gmail.com>
2025-05-16 10:48:08 +0800

    modify 4

--- lesson4/index.py
+++ lesson4/index.py
@@ -1,8 +1,16 @@
 # print("Hello! Flask!")
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__) 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index(): 
+    return "<h1>Hello, Flask!</h1><p>這是我的第1頁</p>"
+
+@app.route("/user")
+def user():
+    return "<h1>user</h1><p>這是我的第2頁</p>"
+
+@app.route("/product")
+def user():
+    return "<h1>product</h1><p>這是我的第3頁</p>"
