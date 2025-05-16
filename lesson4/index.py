a87c14b31fa16cd9e7d7145cdc01458975c3d885
Chiwei-star <panda2008k@gmail.com>
2025-05-16 15:04:52 +0800

    modify 5

--- lesson4/index.py
+++ lesson4/index.py
@@ -1,16 +1,16 @@
 # print("Hello! Flask!")
-from flask import Flask
+from flask import Flask,render_template
 
-app = Flask(__name__) 
+app = Flask(__name__)
 
 @app.route("/")
-def index(): 
-    return "<h1>Hello, Flask!</h1><p>這是我的第1頁</p>"
+def index():
+    return render_template("index.html")
 
 @app.route("/user")
 def user():
-    return "<h1>user</h1><p>這是我的第2頁</p>"
+    return "<h1>user!</h1><p>這是我的第2頁</p>"
 
 @app.route("/product")
-def user():
-    return "<h1>product</h1><p>這是我的第3頁</p>"
+def product():
+    return "<h1>product!</h1><p>這是我的第3頁</p>"
