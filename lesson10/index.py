0dd9bf7912d0279168a47336b9be94028fe3bbc2
Chiwei-star <panda2008k@gmail.com>
2025-06-09 15:36:16 +0800

    modify 3

--- lesson10/index.py
+++ lesson10/index.py
@@ -14,32 +14,59 @@ app = Flask(__name__)
 def index():
     return render_template("index.html.jinja2")
 
-@app.route("/classes")
-def classes():
-    name = "Robert"
-    weekdays = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"]
-    return render_template("classes.html.jinja2",name=name,weekdays=weekdays)
+@app.route("/classes",defaults={'course_types':'一般課程'})
+@app.route("/classes/<course_types>")
+def classes(course_types):
+    print(course_types)
+    conn = psycopg2.connect(conn_string)
+    with conn.cursor() as cur:
+        sql = """
+        SELECT DISTINCT "課程類別" FROM "進修課程";
+        """
+        cur.execute(sql)
+        temps = cur.fetchall()
+        kinds = [kind[0] for kind in temps]
+        kinds.reverse()
+
+        sql_course = f"""
+        SELECT
+            "課程名稱",
+            "老師",
+            "進修人數",
+            "報名開始日期",
+            "報名結束日期",
+            "課程內容",
+            "進修費用"
+        FROM
+            "進修課程"
+        WHERE
+            "課程類別" = '{course_types}'
+        LIMIT 6;
+        """
+        cur.execute(sql_course)
+        course_data = cur.fetchall()
+        print(course_data)
+
+    return render_template("classes.html.jinja2",kinds=kinds,course_data=course_data)
 
 @app.route("/new")
 def new():
     try:
         conn = psycopg2.connect(conn_string)
         with conn.cursor() as cur:
-            sql = "SELECT * FROM 最新訊息"
             sql = """SELECT * FROM public.最新訊息
                      ORDER BY 上版日期 desc"""
             cur.execute(sql)
-            # 取得所有資料
+        # 取得所有資料
             rows = cur.fetchall()
+            
         
-        #raise Exception("出現錯誤")
-        #print("連線成功")
     except OperationalError as e:
         print("連線失敗")
         print(e)
-        return render_template('error.html.jinja2', error_message="資料庫錯誤"), 500
+        return render_template("error.html.jinja2",error_message="資料庫錯誤"),500
     except:
-        return render_template('error.html.jinja2', error_message="不知名錯誤"), 500
+        return render_template("error.html.jinja2",error_message="不知名錯誤"),500
     conn.close()
     return render_template("new.html.jinja2",rows=rows)
 
@@ -50,4 +77,3 @@ def traffic():
 @app.route("/contact")
 def contact():
     return render_template("contact.html.jinja2")
-
